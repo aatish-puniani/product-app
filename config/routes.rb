a5acd32c81b85e1896daf1f3fbfeedc561e91a45
aatish-puniani <inainup@gmail.com>
2016-03-20 16:58:47 -0500

    order controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   get '/products/:id/edit' => 'products#edit'
   patch '/products/:id' => 'products#update'
   delete '/products/:id' => 'products#destroy'
+
+  get '/orders/new' => 'orders#new'
+  post '/orders' => 'orders#create'
+  get '/orders/:id' => 'orders#show'
+  get '/orders/:id/edit' => 'orders#edit'
+  patch '/orders/:id' => 'orders#update'
+  delete '/orders/:id' => 'orders#destroy'
 end
